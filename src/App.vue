d7e08f4cd39fed6353b935616735d12b57e8a14d
JamPeng <pengzhe.info@gmail.com>
2022-05-23 21:34:55 +0800

    製作登入頁面

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <button type="button" class="btn btn-primary">Primary</button>
+
   <router-view />
 </template>
 
